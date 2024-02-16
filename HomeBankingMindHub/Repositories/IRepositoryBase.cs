6b80a102cb8718e4c3e573bf54e6399629bab766
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-02-16 15:02:16 -0300

    Creacion de clase RepositoryBase que implementa la interfaz Repository base

--- HomeBankingMindHub/Repositories/IRepositoryBase.cs
+++ HomeBankingMindHub/Repositories/IRepositoryBase.cs
@@ -1,6 +1,16 @@
-﻿namespace HomeBankingMindHub.Repositories
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace HomeBankingMindHub.Repositories
 {
-    public interface IRepositoryBase
+    public interface IRepositoryBase<T>
     {
+        IQueryable<T> FindAll();
+        IQueryable<T> FindAll(Func<IQueryable<T>, IIncludableQueryable<T, object>> includes = null);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> condition);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void SaveChanges();
     }
 }
---
+++ HomeBankingMindHub/Repositories/RepositoryBase.cs
@@ -0,0 +1,60 @@
+﻿using HomeBankingMindHub.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace HomeBankingMindHub.Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected HomeBankingContext RepositoryContext {  get; set; }
+
+        protected RepositoryBase(HomeBankingContext repositoryContext)
+        {
+            RepositoryContext = repositoryContext;
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            //return RepositoryContext.Set<T>().AsNoTracking();
+            return RepositoryContext.Set<T>().AsNoTrackingWithIdentityResolution();
+        }
+
+        public IQueryable<T> FindAll(Func<IQueryable<T>, IIncludableQueryable<T, object>> includes = null)
+        {
+            IQueryable<T> queryable = RepositoryContext.Set<T>();
+
+            if (includes != null)
+            {
+                queryable = includes(queryable);
+            }
+
+            return queryable.AsNoTrackingWithIdentityResolution();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> condition)
+        {
+            return RepositoryContext.Set<T>().Where(condition).AsNoTrackingWithIdentityResolution();
+        }
+
+        public void Create(T entity)
+        {
+            RepositoryContext.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            RepositoryContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            RepositoryContext.Set<T>().Remove(entity);
+        }
+
+        public void SaveChanges()
+        {
+            RepositoryContext.SaveChanges();
+        }
+    }
+}
