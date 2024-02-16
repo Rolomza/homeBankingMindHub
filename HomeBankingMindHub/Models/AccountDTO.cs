bad16906a696129cea1778691447aab61fe94dea
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-02-16 15:36:35 -0300

    Creacion de DTOS para Client y Account

---
+++ HomeBankingMindHub/Models/AccountDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeBankingMindHub.Models
+{
+    public class AccountDTO
+    {
+        public long Id { get; set; }
+        public string Number { get; set; }
+        public DateTime CreationDate { get; set; }
+        public double Balance { get; set; }
+    }
+}
