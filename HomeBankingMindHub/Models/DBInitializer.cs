a6a53355f6f555e44f858b2fa4055d9b4f0c52c1
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-02-19 12:33:50 -0300

    Pueblo la DB con algunas transacciones inciales

--- HomeBankingMindHub/Models/DBInitializer.cs
+++ HomeBankingMindHub/Models/DBInitializer.cs
@@ -80,6 +80,28 @@
                     context.SaveChanges();
                 }
             }
+
+            if (!context.Transactions.Any())
+            {
+                var account1 = context.Accounts.FirstOrDefault(account => account.Number == "VIN001");
+
+                if (account1 != null)
+                {
+                    var transactions = new Transaction[]
+                    {
+                        new Transaction { AccountId= account1.Id, Amount = 10000, Date= DateTime.Now.AddHours(-5), Description = "Transferencia reccibida", Type = TransactionType.CREDIT },
+                        new Transaction { AccountId= account1.Id, Amount = -2000, Date= DateTime.Now.AddHours(-6), Description = "Compra en tienda mercado libre", Type = TransactionType.DEBIT },
+                        new Transaction { AccountId= account1.Id, Amount = -3000, Date= DateTime.Now.AddHours(-7), Description = "Compra en tienda xxxx", Type = TransactionType.DEBIT },
+                    };
+
+                    foreach (Transaction transaction in transactions)
+                    {
+                        context.Transactions.Add(transaction);
+                    }
+
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
