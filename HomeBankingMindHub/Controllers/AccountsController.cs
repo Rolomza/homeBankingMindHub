195604f054b60877517a32178539b3646f7e12f7
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-02-19 14:21:09 -0300

    Endpoint para traer todas las cuentas y modificacion al TransactionDTO

---
+++ HomeBankingMindHub/Controllers/AccountsController.cs
@@ -0,0 +1,57 @@
+﻿using HomeBankingMindHub.Models;
+using HomeBankingMindHub.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBankingMindHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountsController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var accounts = _accountRepository.GetAllAccounts();
+                var accountsDTO = new List<AccountDTO>();
+
+                foreach (Account account in accounts)
+                {
+                    var newAccountDTO = new AccountDTO
+                    {
+                        Id = account.Id,
+                        Number = account.Number,
+                        CreationDate = account.CreationDate,
+                        Balance = account.Balance,
+                        Transactions = account.Transactions.Select(transaction =>
+                            new TransactionDTO
+                            {
+                                Id = transaction.Id,
+                                Type = transaction.Type.ToString(),
+                                Amount = transaction.Amount,
+                                Description = transaction.Description,
+                                Date = transaction.Date,
+                            }).ToList()
+                    };
+
+                    accountsDTO.Add(newAccountDTO);
+                }
+
+                return Ok(accountsDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
--- HomeBankingMindHub/Models/TransactionDTO.cs
+++ HomeBankingMindHub/Models/TransactionDTO.cs
@@ -3,10 +3,9 @@
     public class TransactionDTO
     {
         public long Id { get; set; }
-        public TransactionType Type { get; set; }
+        public string Type { get; set; }
         public double Amount { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
-        public Account Account { get; set; }
     }
 }
