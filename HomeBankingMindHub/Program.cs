4404ef599fcbf55c817f779f4a1aef93228adb20
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-02-23 14:30:42 -0300

    Se modifica el accounts.html para visualizar correctamente los prestamos

--- HomeBankingMindHub/Program.cs
+++ HomeBankingMindHub/Program.cs
@@ -60,11 +60,11 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
+    //app.UseSwaggerUI(options =>
+    //{
+    //    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    //    options.RoutePrefix = string.Empty;
+    //});
 } else
 {
     app.UseExceptionHandler("/Error");
