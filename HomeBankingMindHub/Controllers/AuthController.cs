082b9e144c0b7dfcd457855f8083cd1b71455da5
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-02-23 13:00:30 -0300

    Agrego archivos para el frontend

--- HomeBankingMindHub/Controllers/AuthController.cs
+++ HomeBankingMindHub/Controllers/AuthController.cs
@@ -2,7 +2,6 @@
 using HomeBankingMindHub.Repositories;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
