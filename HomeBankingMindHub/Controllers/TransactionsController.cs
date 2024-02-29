db0e46a3042853cbca01a448160b357bdcd0c6fc
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-02-29 15:49:17 -0300

    Agrego frontend nuevo

---
+++ HomeBankingMindHub/Controllers/TransactionsController.cs
@@ -0,0 +1,38 @@
+﻿using HomeBankingMindHub.Models.DTOs;
+using HomeBankingMindHub.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBankingMindHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly IClientRepository _clientRepository;
+        private readonly IAccountRepository _accountRepository;
+        private readonly ITransactionRepository _transactionRepository;
+
+        public TransactionsController(IClientRepository clientRepository, IAccountRepository accountRepository,
+            ITransactionRepository transactionRepository)
+        {
+            _clientRepository = clientRepository;
+            _accountRepository = accountRepository;
+            _transactionRepository = transactionRepository;
+        }
+
+        [HttpPost]
+        public IActionResult MakeTransaction([FromBody] TransferDTO newTransfer)
+        {
+            try
+            {   
+                return Ok(newTransfer);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        
+    }
+}
