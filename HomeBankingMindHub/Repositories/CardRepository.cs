a5df7176c95a654198ed5b5bf652d87ae1d1e9d5
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-02-29 09:34:31 -0300

    Creacion de repositorio para tarjetas

---
+++ HomeBankingMindHub/Repositories/CardRepository.cs
@@ -0,0 +1,37 @@
+﻿using HomeBankingMindHub.Models;
+
+namespace HomeBankingMindHub.Repositories
+{
+    public class CardRepository : RepositoryBase<Card>, ICardRepository
+    {
+        public CardRepository(HomeBankingContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<Card> GetAllCards()
+        {
+            return FindAll().ToList();
+        }
+
+        public Card FindById(long id)
+        {
+            return FindByCondition(card => card.Id == id).FirstOrDefault();
+        }
+
+        public Card FindByNumber(string number)
+        {
+            return FindByCondition(card => card.Number == number).FirstOrDefault();
+        }
+
+        public IEnumerable<Card> GetCardsByClient(long clientId)
+        {
+            return FindByCondition(card => card.ClientId == clientId).ToList();
+        }
+
+        public void Save(Card card)
+        {
+            Create(card);
+            SaveChanges();
+        }
+    }
+}
---
+++ HomeBankingMindHub/Repositories/ICardRepository.cs
@@ -0,0 +1,13 @@
+﻿using HomeBankingMindHub.Models;
+
+namespace HomeBankingMindHub.Repositories
+{
+    public interface ICardRepository
+    {
+        IEnumerable<Card> GetAllCards();
+        void Save(Card card);
+        Card FindById(long id);
+        IEnumerable<Card> GetCardsByClient(long clientId);
+        Card FindByNumber(string number);
+    }
+}
