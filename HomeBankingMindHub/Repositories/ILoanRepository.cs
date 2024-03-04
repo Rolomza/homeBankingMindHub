859bab79cf76a71090063a5afdd4f3704cdcee86
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-03-04 15:56:14 -0300

    Creacion de repositorios para Loan

---
+++ HomeBankingMindHub/Repositories/ILoanRepository.cs
@@ -0,0 +1,10 @@
+﻿using HomeBankingMindHub.Models;
+
+namespace HomeBankingMindHub.Repositories
+{
+    public interface ILoanRepository
+    {
+        IEnumerable<Loan> GetAll();
+        Loan FindById(long id);
+    }
+}
---
+++ HomeBankingMindHub/Repositories/LoanRepository.cs
@@ -0,0 +1,21 @@
+﻿using HomeBankingMindHub.Models;
+
+namespace HomeBankingMindHub.Repositories
+{
+    public class LoanRepository : RepositoryBase<Loan>, ILoanRepository
+    {
+        public LoanRepository(HomeBankingContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+        public Loan FindById(long id)
+        {
+            return FindByCondition(loan => loan.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Loan> GetAll()
+        {
+            return FindAll().ToList();
+        }
+    }
+}
