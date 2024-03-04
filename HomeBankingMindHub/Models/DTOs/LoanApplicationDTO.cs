f8ec09958692185b10c9286ee5538269c226db99
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-03-04 15:42:55 -0300

    Creo DTOs para Loan

---
+++ HomeBankingMindHub/Models/DTOs/LoanApplicationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeBankingMindHub.Models.DTOs
+{
+    public class LoanApplicationDTO
+    {
+        long LoanId { get; set; }
+        double Amount { get; set; }
+        string Payments { get; set; }
+        string ToAccountNumber { get; set; }
+    }
+}
---
+++ HomeBankingMindHub/Models/DTOs/LoanDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeBankingMindHub.Models.DTOs
+{
+    public class LoanDTO
+    {
+        long Id { get; set; }
+        string Name { get; set; }
+        double MaxAmount { get; set; }
+        string Payments { get; set; }
+    }
+}
