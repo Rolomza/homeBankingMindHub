72b609136f92a0d6fed129edab4b8bab50bc0b78
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-03-07 12:45:59 -0300

    Creo carpeta de servicios, creo interfaz IClientService y su clase de implementacion

---
+++ HomeBankingMindHub/Services/IClientService.cs
@@ -0,0 +1,9 @@
+﻿using HomeBankingMindHub.Models.DTOs;
+
+namespace HomeBankingMindHub.Services
+{
+    public interface IClientService
+    {
+        ClientDTO getClientDTOByEmail(string email);
+    }
+}
---
+++ HomeBankingMindHub/Services/Impl/ClientService.cs
@@ -0,0 +1,21 @@
+﻿using HomeBankingMindHub.Models;
+using HomeBankingMindHub.Models.DTOs;
+using HomeBankingMindHub.Repositories;
+
+namespace HomeBankingMindHub.Services.Impl
+{
+    public class ClientService : IClientService
+    {
+        private readonly IClientRepository _clientRepository;
+
+        public ClientService(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+        public ClientDTO getClientDTOByEmail(string email)
+        {
+            Client client = _clientRepository.FindByEmail(email);
+            return new ClientDTO(client);
+        }
+    }
+}
